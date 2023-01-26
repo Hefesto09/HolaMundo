7b424d97c70477cd0c42642533817d8bab1fa50b
Victor <victor090598rosales[200~@~gmail.com>
2023-01-26 06:57:03 -0500

    java_prac_1

--- src/Classe_math.java
+++ src/Classe_math.java
@@ -1,11 +1,11 @@
 public class Classe_math {
     public static void main(String[] args) {
-        double raiz = Math.sqrt(9);
+        double raiz1 = Math.sqrt(9);
 
         double base = 5, exponente = 2; 
         double resultado = Math.pow(base, exponente);
 
         System.out.println(resultado);
-        System.out.println(raiz);
+        System.out.println(raiz1);
     }
 }
